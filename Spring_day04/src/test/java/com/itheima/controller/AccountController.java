e073589d48ac52a0a3d335cd328d5cc0bfbe4560
liujt522 <1130458320@qq.com>
2019-06-13 21:19:37 +0800

    把本地工程添加到git

---
+++ Spring_day04/src/test/java/com/itheima/controller/AccountController.java
@@ -0,0 +1,21 @@
+package com.itheima.controller;
+
+import com.itheima.service.AccountService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:applicationContext1.xml")
+public class AccountController {
+    @Test
+    public void Test(){
+        ApplicationContext app = new ClassPathXmlApplicationContext("applicationContext1.xml");
+        AccountService accountService = app.getBean(AccountService.class);
+        accountService.transfer("zhangsan","lisi",500);
+    }
+
+}
---
+++ Spring_day04/src/test/java/com/itheima/service/impl/AccountServiceImpl.java
@@ -0,0 +1,22 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.AccountDao;
+import com.itheima.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service("accountService")
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountDao accountDao;
+   /* public void setAccountDao(AccountDao accountDao) {
+        this.accountDao = accountDao;
+    }*/
+
+    public void transfer(String outMan, String inMan, double money) {
+        accountDao.out(outMan,money);
+       // int i = 1/0;
+        accountDao.in(inMan,money);
+    }
+}
